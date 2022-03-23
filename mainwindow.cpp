4fe7ce8cbabdd85ee68253cbc69f761d85ffff26
fdelac01 <fdelac01@estudiantes.unileon.es>
2022-03-23 21:20:36 +0100

    El código del factorial más o menos, hay que adaptarlo a lo nuestro

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+///ESTO DENTRO DEL SLOT DE OK
+//using namespace std;
+//int main(void){
+//   int i;
+//   int fact=1;
+//   int numero;
+//   cout<<"ingresa un numero: "; ///AQUI EN VEZ DE COUT HAY Q LEER LO QUE PILLEMOS POR EL LINEEDIT Y METERLO AQUI
+//   cin>>numero;
+//   if(numero<0) fact =0;
+//   else if(numero==0) fact=1;
+//   else{
+//      for (i = 1; i <= numero; i++){
+//         fac = fact*i;
+//      }
+//   }
